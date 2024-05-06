f3832db7c5b22903d9a02b84c87f27b2ece58826
EricOrtegaGi <ericortega@iesebre.com>
2024-05-06 22:24:25 +0200

    No acabat

--- Sales/models/report.py
+++ Sales/models/report.py
@@ -1,16 +1,20 @@
 # report.py
 from odoo import models
 
+
 class ReportSaleOrder(models.AbstractModel):
     _name = 'report.sale.report_saleorder'
     _description = 'Sales Order Report'
 
     def _get_report_values(self, docids, data=None):
         docs = self.env['sale.order'].browse(docids)
-        return {
-            'doc_ids': docs.ids,
-            'doc_model': 'sale.order',
-            'docs': docs,
-            'num_articles': docs.num_articles,
-            'preferred_email': docs.preferred_email,
-        }
+        report_values = []
+        for doc in docs:
+            report_values.append({
+                'doc_ids': doc.ids,
+                'doc_model': 'sale.order',
+                'docs': doc,
+                'num_articles': doc.num_articles,
+                'preferred_email': doc.preferred_email,
+            })
+        return report_values
--- custom_report/models/module_report.py
+++ custom_report/models/module_report.py
@@ -1,30 +1,44 @@
-from odoo import fields, models
+from odoo import api, fields, models
 from odoo import tools
 
-class ModuleReport(models.Model):
-    _name = 'custom.report'
-    _description = "Module Report"
-    #_rec_name = 'module_field'
-    _auto = False
-    
-    total_price = fields.Float('Total Price', readonly=True)
-    comercial = fields.Char('Comercial', readonly=True)
-    state = fields.Char()   #fields.Selection([('New','En Tractament'),('OfferAccepted', 'Oferta Acceptada'),('Sold', 'Venuda'),('Canceled', 'Cancel·lada')],copy=False, readonly=True)
-    
-    
-    def _select(self):
-        return "SELECT SUM(epo.price) AS total_price, rp.name as comercial, ep.state, ep.id"
 
-    def _group_by(self):
-        return "GROUP BY rp.name, ep.state, ep.id"
+class SalesModuleReport(models.Model):
+    _name = 'sales.module.report'
+    _description = "Sales Module Report"
+    _auto = False
+
+    num_articles = fields.Integer('Numero de articulos', readonly=True)
+    preferred_email = fields.Char('Preferred Email', readonly=True)
+
+    def print_report(self):
+        return self.env.ref('custom_report.report_template').report_action(self)
+
+    def _select(self):
+        return """
+            SELECT
+                so.id,
+                COUNT(sol.id) AS num_articles,
+                rp.email AS preferred_email
+        """
 
     def _from(self):
-        return "FROM estate_property ep LEFT JOIN res_users ru ON ep.salesperson_id = ru.id LEFT JOIN res_partner rp ON ru.partner_id = rp.id LEFT JOIN estate_property_offer epo ON ep.id = epo.property_id"
+        return """
+            FROM sale_order so
+            JOIN sale_order_line sol ON so.id = sol.order_id
+            JOIN res_partner rp ON so.partner_id = rp.id
+        """
+
+    def _group_by(self):
+        return """
+            GROUP BY so.id, rp.email
+        """
 
     def init(self):
         tools.drop_view_if_exists(self.env.cr, self._table)
-        self.env.cr.execute("""CREATE or REPLACE VIEW %s as (
-            %s
-            %s
-            %s
-        )""" % (self._table, self._select(), self._from(), self._group_by()))
+        self.env.cr.execute("""
+            CREATE or REPLACE VIEW %s as (
+                %s
+                %s
+                %s
+            )
+        """ % (self._table, self._select(), self._from(), self._group_by()))
